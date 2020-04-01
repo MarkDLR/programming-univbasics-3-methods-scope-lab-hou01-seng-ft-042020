f86d0d569031574330202e7205c5c3d06772982f
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-01 20:38:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,12 +5,16 @@ def all_phrases(name)
   
 
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link
+  phrase = 
 end
